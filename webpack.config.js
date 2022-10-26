1aa48fd2577e60223691fb6c3061dd62ab47c740
joker876 <joker876x@gmail.com>
2022-10-26 12:42:02 +0200

    tidy the repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/index.js',
-  mode: 'production',
-  output: {
-    filename: '[name].min.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'BetterTurtle',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    auxiliaryComment: 'Turtle JS redefined',
-  },
+    entry: './dist/index.js',
+    mode: 'production',
+    output: {
+        filename: 'simple-turtle.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'SimpleTurtle',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+        auxiliaryComment: 'TurtleJS simplified',
+    },
 };
